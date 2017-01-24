856b01f48ebc6f334b7e7c7a91f591cccf7012a4
chipoglesby <chip@creativesparkcolumbia.com>
2017-01-23 19:19:02 -0700

    Updating earnings

--- code/functions.R
+++ code/functions.R
@@ -2,14 +2,14 @@
 
 # ---- clean ----
 # Give the data better names
-names(salaries) <- c("jobTitle", "department", "earnings2015")
+names(salaries) <- c("jobTitle", "department", "earnings")
 
 # Lowercase all names for consistency
 salaries %<>%
   transmute(jobTitle = tolower(jobTitle),
             department = tolower(department),
-            earnings2015 = 
-              as.numeric(gsub('(\\$)|(\\,)','', earnings2015)))
+            earnings = 
+              as.numeric(gsub('(\\$)|(\\,)','', earnings)))
 
 ## Next, let's ask some questions
 
@@ -17,7 +17,7 @@ salaries %<>%
 # Who is the highest paid employye in each department?
 highest <- salaries %>% 
   group_by(department) %>% 
-  filter(earnings2015 == max(earnings2015))
+  filter(earnings == max(earnings))
 
 ## Let's look at one specific department, like police
 # ---- police ----
@@ -30,14 +30,14 @@ police <- salaries %>%
 police %>% 
   group_by(department) %>%
   summarize(
-    minsalaries = min(earnings2015),
-    mediansalaries = median(earnings2015),
-    maxsalaries = max(earnings2015),
+    minsalaries = min(earnings),
+    mediansalaries = median(earnings),
+    maxsalaries = max(earnings),
     count = n()) %>% 
   arrange(desc(mediansalaries))
 
 police %>% 
-  ggplot(aes(earnings2015)) +
+  ggplot(aes(earnings)) +
   geom_histogram(bins = 100)
 
 # ---- policeCount ----
@@ -48,24 +48,24 @@ police %>%
 
 # ---- generalWork ----
 # salaries %>%
-#   ggplot(aes(department, median(earnings2015))) +
+#   ggplot(aes(department, median(earnings))) +
 #   geom_bar(stat = identity)
 
 
 median <- salaries %>%
   group_by(department) %>% 
   summarize(positionCount = length(jobTitle),
-            mediansalaries = round(median(earnings2015), 2)) %>% 
+            mediansalaries = round(median(earnings), 2)) %>% 
   arrange(desc(mediansalaries))
 
 median <- salaries %>%
   group_by(department, jobTitle) %>%
   summarize(positionCount = length(jobTitle),
-            mediansalaries = round(median(earnings2015), 2)) %>% 
+            mediansalaries = round(median(earnings), 2)) %>% 
   arrange(department, desc(mediansalaries))
 
 # salaries %>% 
 #   group_by(department) %>% 
-#   summarize(total = sum(earnings2015)) %>% 
+#   summarize(total = sum(earnings)) %>% 
 #   ggplot(aes(department, total)) +
 #   geom_bar(stat = identity)
