3afa4a488b432c167caaeefd98ca0dab7f2c2d79
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-01-24 18:30:15 -0700

    Updating code

--- code/functions.R
+++ code/functions.R
@@ -17,38 +17,41 @@ salaries %<>%
       replace(serviceArea,grepl("",serviceArea), "unknown"),
     serviceArea = 
       replace(serviceArea,grepl("office of the chief|patrol|investigative|\
-                                information services|investigations",department), "police services"),
+      information services|investigations",department), "police services"),
     serviceArea = 
       replace(serviceArea,grepl("accounting and treasury|budget|revenue|\
-                                purchasing|risk mgmt|airport|safety|finance admin",department),
-              "financial services"),
+      purchasing|risk mgmt|airport|safety|finance admin",department),
+      "financial services"),
     serviceArea = 
       replace(serviceArea,grepl("information technology|human resources|\
-                                public involvement",department), "information and employee services"),
+      public involvement",department), "information and employee services"),
     serviceArea = 
       replace(serviceArea,grepl("water|electric|utility|customer connections|\
-                                ^ut|^l&p",department), "utility services"),
+      ^ut|^l&p",department), "utility services"),
     serviceArea = 
       replace(serviceArea,grepl("economic health|^environmental|social \
-                                sustainability|sustainability services admin",department), 
-              "sustainability services"),
+      sustainability|sustainability services admin",department), 
+      "sustainability services"),
     serviceArea = 
       replace(serviceArea,grepl("cultural services|recreation|parks|natural \
-                                areas|park planning|operation services|community services admin", 
-                                department), "community and operation services"),
+      areas|park planning|operation services|community services admin", 
+      department), "community and operation services"),
     serviceArea = 
       replace(serviceArea,grepl("^city manager\'s",department), "city manager"),
     serviceArea = 
       replace(serviceArea,grepl("city attorney",department), "city attorney"),
     serviceArea = 
       replace(serviceArea,grepl("comm dev|streets|engineering|fc moves|transfort|\
-                                ^traffic$|parking|^pdt",department), "planning development & transportation"),
+      ^traffic$|parking|^pdt",department), "planning development & transportation"),
     serviceArea =
       replace(serviceArea,grepl("city clerk",department), "deputy city manager"),
     serviceArea = 
       replace(serviceArea,grepl("library",department), "library services"),
     serviceArea = 
-      replace(serviceArea,grepl("fire",department), "poudre fire authority")
+      replace(serviceArea,grepl("fire",department), "poudre fire authority"),
+    hourlyWage = 
+      ifelse(earnings > 17056, as.numeric(round(earnings/2080, 2)), NA),
+    status = ifelse(is.na(hourlyWage) == TRUE, "part time", "full time")
       )
 
 unknown <- salaries %>% 
@@ -68,7 +71,14 @@ highest <- salaries %>%
 
 # Subsetting police
 police <- salaries %>%
-  filter(serviceArea == 'police services')
+  filter(serviceArea == 'police services') %>% 
+  group_by(jobTitle) %>% 
+  summarize(
+            employeeCount = n(),
+            medianSalary = median(earnings),
+            meanSalary = mean(earnings),
+            maxSalary = max(earnings),
+            minSalary = min(earnings))
 
 police %>% 
   group_by(department) %>%
